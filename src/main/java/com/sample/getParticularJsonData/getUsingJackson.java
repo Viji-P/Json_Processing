be494e6bacf76e59b74f170d30a1920bc0ebe63f
Viji-P <mpvijayalakshmi91103@gmail.com>
2023-09-17 12:43:58 +0530

    some modified

--- src/main/java/com/sample/getParticularJsonData/getUsingJackson.java
+++ src/main/java/com/sample/getParticularJsonData/getUsingJackson.java
@@ -2,6 +2,8 @@ package com.sample.getParticularJsonData;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,12 +15,21 @@ public class getUsingJackson {
         try {
             JsonNode node=mapper.readTree(new File("src//DisposalInstructionStep.json"));
 
+            Map<String,Object> dm=new HashMap<>();
+
             for(JsonNode node2:node.get("data")){
 
                 for(JsonNode node3:node2.get("FG_TRD_PURPOSE_CODE")){
 
-                    System.out.println(node3.get("AMT").asInt());
+                    int amount=node3.get("AMT").asInt();
+
+                    String identifier = node3.get("ID").asText(); 
+
+                    dm.put(identifier,amount);
                 }
+            }
+            for(Map.Entry<String,Object> entry:dm.entrySet()){
+                System.out.println(entry.getValue());
             }
         } catch (IOException e) {
             
