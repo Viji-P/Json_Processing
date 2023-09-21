def86b8956a329016c4edea8c9867082e54aa5c4
Viji-P <mpvijayalakshmi91103@gmail.com>
2023-09-21 17:17:32 +0530

    using stream to extract data

--- src/main/java/com/sample/getParticularJsonData/UsingStream.java
+++ src/main/java/com/sample/getParticularJsonData/UsingStream.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -40,12 +40,13 @@ public class UsingStream {
         try {
             List<Map<String, Object>> dMap = (List<Map<String, Object>>) jsonMap.get("data");
 
-            dMap.stream()
+            List<Object> collectedList =  dMap.stream()
                 .map(dataNode -> (List<Map<String, Object>>) dataNode.get("FG_TRD_PURPOSE_CODE"))
                 .flatMap(List::stream)
                 .map(purposeCode -> purposeCode.get("AMT"))
-                .forEach(System.out::println);
+                .collect(Collectors.toList());
 
+                System.out.println(collectedList);
         } catch (Exception e) {
 
             e.printStackTrace();
